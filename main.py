fd491aae761ddc1598bd4039252c1cd413fc23df
Brody Moore <bammoore511@gmail.com>
2021-09-30 17:41:02 -0400

    added stock price API get request

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
+import os
+import requests
+import datetime as dt
+from dotenv import load_dotenv
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+
+STOCK_API_ENDPOINT = "https://www.alphavantage.co/query"
+
+# load and instantiate environment variables
+load_dotenv(".env")
+news_api_key = os.getenv("news_api_key")
+alpha_stock_key = os.getenv("alpha_stock_key")
+twilio_sid = os.getenv("twilio_sid")
+twilio_auth_token = os.getenv("twilio_auth_token")
 
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+stock_parameters = {
+    'function': 'TIME_SERIES_DAILY_ADJUSTED',
+    'symbol': STOCK,
+    'apikey': alpha_stock_key,
+}
+tesla = requests.get(STOCK_API_ENDPOINT, params=stock_parameters)
+tesla.raise_for_status()
+tesla_data = tesla.json()
+
+
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
